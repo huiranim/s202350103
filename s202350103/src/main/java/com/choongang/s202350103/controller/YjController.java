d5622fdd6cdc04862e40c06bb38b9e93c2c38955
ayj8487 <ayj8487@naver.com>
2023-10-27 19:48:13 +0900

    ㅎㅇㅎㅇ

---
+++ s202350103/src/main/java/com/choongang/s202350103/controller/YjController.java
@@ -0,0 +1,12 @@
+package com.choongang.s202350103.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class YjController {
+
+	YjController(){
+		System.out.println("sadsa");
+	}
+
+}
