5e3ad922ccbbc829929f7abdad7873b38daabd58
admin <admin@DESKTOP-2O3K7EH>
2023-10-30 11:33:29 +0900

    희라 주말 작업 건

---
+++ s202350103/src/main/java/com/choongang/s202350103/hrDao/OrderDao.java
@@ -0,0 +1,17 @@
+package com.choongang.s202350103.hrDao;
+
+import java.util.List;
+import com.choongang.s202350103.model.Orderr;
+
+public interface OrderDao {
+	// BO 주문목록
+	// boOrderList.jsp
+	int 		 countOrderrList();
+	List<Orderr> selectOrderrList(Orderr orderr);
+	int 		 minNbNum(long o_order_num);
+	String 		 selectNBTitleByNum(int nb_num);
+	String 		 selectOBTitleByNum(int nb_num);
+
+	
+
+}
