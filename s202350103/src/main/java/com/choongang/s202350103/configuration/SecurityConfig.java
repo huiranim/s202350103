728060e72da098f6959a5ca876f1808c1c2822ff
admin <admin@DESKTOP-UU4I217>
2023-10-31 15:03:57 +0900

    용빈 - index.html(x) -> main.jsp(o) 	  headerFo 수정 및 Logout

--- s202350103/src/main/java/com/choongang/s202350103/configuration/SecurityConfig.java
+++ s202350103/src/main/java/com/choongang/s202350103/configuration/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.choongang.s202350103.configuration;
 
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.firewall.DefaultHttpFirewall;
-import org.springframework.security.web.firewall.HttpFirewall;
+
 
 @Configuration
 public class SecurityConfig {
@@ -28,12 +26,6 @@ public class SecurityConfig {
 			.permitAll();
 		
 		return http.build();
-	}
-	
-	// 페이지 이동 시 // 관련 오류 
-	@Bean
-	public HttpFirewall defaultHttpFirewall() {
-		return new DefaultHttpFirewall();
 	}
 	
 
--- s202350103/src/main/java/com/choongang/s202350103/controller/YbController.java
+++ s202350103/src/main/java/com/choongang/s202350103/controller/YbController.java
@@ -23,46 +23,58 @@ import lombok.extern.slf4j.Slf4j;
 public class YbController {
 
 	private final MemberService ms;
-	
+	// Main Page
 	@RequestMapping(value = "/")
 	public String main() {
+		System.out.println("YbController main start... ");
 		return "main";
 	}
-	
-	
+	// 로그인 창 이동
 	@GetMapping(value = "loginForm")
 	public String loginForm() {
 		return "yb/loginForm";
 	}
-	
-	@RequestMapping(value = "userLogin", method = RequestMethod.POST)
-	public String login(Member member1, HttpSession session, HttpServletRequest request) {
+	// 로그인
+	@RequestMapping(value = "memberLogin")
+	public String login(Member member1, HttpSession session, HttpServletRequest request, Model model) {
 		log.info("Login page");
+
+		System.out.println("YbController login() session -> " + session);
+		Member member = ms.login(member1);
 		
 
-	    System.out.println("YbController login() session -> " + session);
-		Member member = ms.login(member1);
-		System.out.println("YbController login() session -> " + session.getId());
-		
-		if(member != null) {
+		if (member != null) {
 			session.setAttribute("member", member);
-			
+			System.out.println("YbController login() session -> " + session.getId());
 			return "redirect:/";
-		} else
+		} else {
+			// 로그인실패
+			model.addAttribute("msg", "로그인실패");
 			return "yb/loginForm";
+		}
 	}
- 
-	   @GetMapping(value = "logout")
+	
+	// 로그아웃
+	@GetMapping(value = "memberLogout")
 	   public String logout(HttpSession session, HttpServletRequest request) {
+			System.out.println("YbController userLogout start... ");
 	      try {
-	         session = request.getSession(false);
-	          if (session != null) {
-	              session.invalidate();
-	          }
-	      } catch (Exception e) {
-	         System.out.println(e.getMessage());
+				
+				  session = request.getSession(false); //세션이 있으면 기존 세션을 반환한다.
+				  // 세션이 없으면 새로운  세션을 생성하지 않고, null을 반환
+				  if (session != null) {
+					  System.out.println("YbController logout session null ");
+					  session.removeAttribute("member"); session.invalidate(); // 세션 초기화
+				  }
+		  } catch (Exception e) {
+	         System.out.println("logout Exception -> "+e.getMessage());
 	      }
-	       return "redirect:/";
-	      }
-	        
+	      return "redirect:/";
+	   }
+	
+	@GetMapping(value = "memberMyPage1")
+	public String memberMyPage1() {
+		System.out.println("YbController memberMyPage1 start...");
+		return "yb/memberMyPage1";
+	}
 }
