3edc020406811af7a890c6e6f149750e0c3dac6f
ayj8487 <ayj8487@naver.com>
2023-11-24 02:55:14 +0900

    영준 문의답글 완

--- s202350103/src/main/java/com/choongang/s202350103/controller/YjController.java
+++ s202350103/src/main/java/com/choongang/s202350103/controller/YjController.java
@@ -3,6 +3,7 @@ package com.choongang.s202350103.controller;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -622,18 +623,6 @@ public class YjController {
 //		  }
 		  
 		  List<Member> memberMyOrder = ms.memberMyOrder(m_num);
-//
-//		  for(Member member : memberMyOrder) {
-//			  int mNum = member.getM_num();
-//			  int orderNum = (int) member.getO_order_num();
-//			  String orderDate = formatDate(member.getO_order_date());
-//			  
-//			  System.out.println(mNum);
-//			  System.out.println(orderNum);
-//			  System.out.println(orderDate);
-//			  
-//			  
-//		  }
 		  
 		  //LinkedHashMap  맵의 키-값 쌍 을 유지 시켜 삽입 순서 보장 -> 키와 값을 순서대로 유지하고 순서기반 접근
 		  Map<String, List<Member>> orderNumGroups = new LinkedHashMap<>();
@@ -823,19 +812,22 @@ public class YjController {
 	  // 문의 상세조회
 	  @GetMapping("/memberQInfo")
 	  public String memberQInfo(@RequestParam int mq_num, 
-			  					String currentPage, Model model, HttpServletRequest request) {
+			  					Model model, HttpServletRequest request) {
 		  
+		  // 문의 상세 조회
 		  MemberQ memberQInfo  = ms.memberQInfo(mq_num);
-		  
+		  // 답글 카운트
+		  int replyCount = ms.replyCount(mq_num);
+		  // 답글 조회
 		  List<MqReply> mqReplyList = ms.mqReplyList(mq_num);
 		  
 		  int mq_hidden = memberQInfo.getMq_hidden();
 		  int m_num = memberQInfo.getM_num();
 		  int m_admin = memberQInfo.getM_admin();
 
-		  System.out.println(mq_hidden);
-		  System.out.println(m_num);
-		  System.out.println(m_admin);
+		  System.out.println("yj con memberQInfo mq_hidden :" +mq_hidden);
+		  System.out.println("yj con memberQInfo m_num :" +m_num);
+		  System.out.println("yj con memberQInfo m_admin :" +m_admin);
 		  
 		  // 비밀글 일 때  
 		  if(mq_hidden == 1) {
@@ -855,6 +847,7 @@ public class YjController {
 			model.addAttribute("recentBookList", recentBookList);
 		  
 		  model.addAttribute("memberQInfo",memberQInfo);
+		  model.addAttribute("replyCount",replyCount);
 		  model.addAttribute("mqReplyList",mqReplyList);
 
 		  return "yj/memberQInfo";
@@ -1066,6 +1059,36 @@ public class YjController {
 		  	System.out.println("yj Ajax m_addr ->" + m_addr);
 			
 			return m_addr;
+	  }
+	  
+	  // 문의 답변 작성
+	  @PostMapping("/replyInsert")
+	  public String replyInsert(@ModelAttribute MqReply reply,@RequestParam int mq_num , Model model) {
+		  
+		  System.out.println(mq_num);
+		  
+		  int replyInsert = ms.replyInsert(reply);
+		  
+		  model.addAttribute("replyInsert",replyInsert);
+		  
+		  return "redirect:/memberQInfo?mq_num="+mq_num;
+	  }
+	  
+	  // 문의 답변 추천
+	  @ResponseBody
+	  @PostMapping("/likeReply")
+	  public Map<String, Object> likeReply(@RequestParam("mqr_num") Long mqr_num) {
+		Map<String, Object> response = new HashMap<>();  	
+
+		try {
+			int likeReply = ms.likeReply(mqr_num);
+			response.put("success", true);
+
+		}catch (Exception e) {
+			response.put("success", false);
+		}
+		
+		return response;
 	  }
 	
 	  
