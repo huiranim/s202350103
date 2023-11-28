8fd1e92da1d40b0232e833f7b1b8b4aacaaefbab
96dyd <96dyd@yongbin>
2023-11-29 01:55:19 +0900

    용빈 - 인터셉터 로그인 후 이전페이지

--- s202350103/src/main/java/com/choongang/s202350103/ybService/AdminAuthorityInterCeptor.java
+++ s202350103/src/main/java/com/choongang/s202350103/ybService/AdminAuthorityInterCeptor.java
@@ -4,12 +4,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.choongang.s202350103.model.Member;
 
 public class AdminAuthorityInterCeptor implements HandlerInterceptor {
+	
+	private static final String LOGIN = "login";
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("LoginInterCeptor postHandle Start...");
+
+		HttpSession session = request.getSession();
+
+		ModelMap modelMap = modelAndView.getModelMap();
+		Object member = modelMap.get("member");
+		Object dest = null;
+		if(member != null) {
+			System.out.println("Login success!");
+			session.setAttribute(LOGIN, member);
+			
+			// 이전 페이지 불러오기
+			dest = session.getAttribute("dest");
+			
+//			response.sendRedirect(dest != null ? dest.toString() : "/");
+			String redirectUrl = dest != null ? dest.toString() : "/";
+			System.out.println("LoginInterCeptor postHandle redirectUrl => " + redirectUrl);
+			
+			session.setAttribute("redirectUrl", redirectUrl);	
+			
+			 session.removeAttribute("dest");  // Clear the 'dest' attribute after using it
+			  
+		}
+	}
+		
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -17,21 +49,44 @@ public class AdminAuthorityInterCeptor implements HandlerInterceptor {
 		// session 객체를 가져옴
         HttpSession session = request.getSession();
         // login처리를 담당하는 사용자 정보를 담고 있는 객체를 가져옴
-        Object obj = session.getAttribute("member");
+//        Object obj = session.getAttribute("member");
         
        	//  비 로그인 시 
-    	if(obj == null) {
-			response.sendRedirect("/loginForm");
-			return false;
+//    	if(obj == null) {
+//    		System.out.println("LoginInterCeptor Start... You do not have permission. Please log in first");
+//
+//    		session.removeAttribute(LOGIN);
+//			response.sendRedirect("/loginForm");
+//			return false;
+//		}
+//        
+//        if(obj != null) {
+//        	// 로그인 후 관리자 아니면 관리자 페이지 접근 X
+//        	Member member = (Member) session.getAttribute("member");
+//        	if(member.getM_admin() != 1) {
+//        		System.out.println("AdminAuthorityInterCeptor Start... You do not have permission. Just Administrator allow");
+//                response.sendRedirect("custom404");
+//                return false; // 더이상 컨트롤러 요청으로 가지 않도록 false로 반환함
+//        	}
+//       
+//        }
+        
+        if(session.getAttribute(LOGIN) == null) {
+    		System.out.println("LoginInterCeptor Start... You do not have permission. Please log in first");
+
+    		session.removeAttribute(LOGIN);
+//			response.sendRedirect("/loginForm");
+//			return false;
 		}
         
-        if(obj != null) {
+        if(session.getAttribute(LOGIN) != null) {
         	// 로그인 후 관리자 아니면 관리자 페이지 접근 X
         	Member member = (Member) session.getAttribute("member");
         	if(member.getM_admin() != 1) {
         		System.out.println("AdminAuthorityInterCeptor Start... You do not have permission. Just Administrator allow");
-                response.sendRedirect("custom404");
-                return false; // 더이상 컨트롤러 요청으로 가지 않도록 false로 반환함
+//                response.sendRedirect("custom404");
+//                return false; // 더이상 컨트롤러 요청으로 가지 않도록 false로 반환함
+        	
         	}
        
         }
@@ -40,13 +95,6 @@ public class AdminAuthorityInterCeptor implements HandlerInterceptor {
         // 따라서 true로하면 컨트롤러 uri로 가게 됨.
 
         return true;
-	}
-	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
 	}
 	
 	
